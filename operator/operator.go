03a57cb4d252dc7470fb7465ae1203d588f42751
Jimmy <rivendinner@gmail.com>
2024-11-10 23:43:51 +0800

    backup

---
+++ operator/operator.go
@@ -0,0 +1,17 @@
+package operator
+
+const (
+	// Comparison operators
+	Equal          = "="
+	NotEqual       = "!="
+	GreaterThan    = ">"
+	LessThan       = "<"
+	GreaterOrEqual = ">="
+	LessOrEqual    = "<="
+	Like           = "LIKE"
+	NotLike        = "NOT LIKE"
+	In             = "IN"
+	NotIn          = "NOT IN"
+	Between        = "BETWEEN"
+	NotBetween     = "NOT BETWEEN"
+)
